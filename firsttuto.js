9ca0c400fd195d14a2f8b4bf46fd148a9e504e75
kishnaalu <kishnaalu78@gmail.com>
2024-05-07 11:56:48 +0545

    sonu

--- firsttuto.js
+++ firsttuto.js
@@ -1,1 +1,8 @@
-alert("hello");
+let college= prompt("enter your college name");
+let course = prompt("enter your course name");
+let stuname = prompt("enter student name");
+
+console.log("good:",college);
+console.log(course);
+console.log(stuname);
+console.log(typeof college)

1aad05abdeffa9aa6a19b23ef1137d5adb722450
kishnaalu <kishnaalu78@gmail.com>
2024-05-07 12:51:39 +0545

    sonu

--- firsttuto.js
+++ firsttuto.js
@@ -1,8 +1,27 @@
-let college= prompt("enter your college name");
-let course = prompt("enter your course name");
-let stuname = prompt("enter student name");
+// let college= prompt("enter your college name");
+// let course = prompt("enter your course name");
+// let stuname = prompt("enter student name");
 
-console.log("good:",college);
-console.log(course);
-console.log(stuname);
-console.log(typeof college)
+// console.log("good:",college);
+// console.log(course);
+// console.log(stuname);
+// console.log(typeof college)
+
+// console.log(`My college name is: ${college}, My course is: ${course} and My student name is: ${stuname}`)
+
+let fname = "sonu"
+let lname= "thapa"
+let combine= fname+lname
+console.log(combine)
+
+ 
+const API_URL="https://localhost:3000"
+const student_url = `${API_URL}/students`
+API_URL="jgkjgker"
+console.log(student_url)
+console.log(API_URL)
+
+console.log(hey)
+var hey="kalika"  // will print 
+
+// mdn for java in firefox
